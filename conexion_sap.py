3da6878a5fd3edad3dea3733e44b8e66f1632e0a
Juan Campos <juan.campos@supralive.remoto>
2026-07-27 08:47:02 -0500

    Cambio de conexion por sap web

--- conexion_sap.py
+++ conexion_sap.py
@@ -1,7 +1,7 @@
 from hdbcli import dbapi
 
 try:
-    cnsap = dbapi.connect(address='10.1.0.70', port=30015, user='SUPRALIVE', password='uGDH6%Yr$K')
+    cnsap = dbapi.connect(address='vm-hana-sprv.heinsohntechcloud.com.co', port=30015, user='SUPRALIVE', password='uGDH6%Yr$K')
     #cnsap = dbapi.connect(address='10.254.254.254', port=30015, user='SVAGRT_DBREADER', password='dPtWhDwV2aRHR5bA')
     print("Conexión exitosa a SAP HANA.")
 except dbapi.Error as e:
